c776c7e8fafcb7932c0aa6f0a8016070f812eba3
Manime016 <lmanikanta016@gmail.com>
2026-02-26 12:48:37 +0530

    Refactored frontend for production: removed localhost, added api config, fixed routing

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -1,9 +1,9 @@
 import { v2 as cloudinary } from "cloudinary";
 
 cloudinary.config({
-  cloud_name: CLOUDINARY_CLOUD_NAME,
-  api_key: 726791459125595,
-  api_secret: L07s9MjP86_MYxajA8wWuE8U6tU,
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_KEY,
+  api_secret: process.env.CLOUD_SECRET,
 });
 
 export default cloudinary;
