d85289209fa01047e5a57fed5c0e486fc028bd05
Manime016 <lmanikanta016@gmail.com>
2026-02-26 10:49:56 +0530

    Refactored frontend for production: removed localhost, added api config, fixed routing

--- ecomm-admin/src/App.jsx
+++ ecomm-admin/src/App.jsx
@@ -1,11 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AddProduct from "./pages/AddProduct";
-
+import MyProducts from "./pages/MyProducts";
+import "./App.css";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        {/* Add Product Page */}
         <Route path="/" element={<AddProduct />} />
+
+        {/* My Products Page */}
+        <Route path="/my-products" element={<MyProducts />} />
       </Routes>
     </BrowserRouter>
   );
