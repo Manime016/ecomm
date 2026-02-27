6ab67d4b60303131a99eebcd6333d187debb2555
Manime016 <lmanikanta016@gmail.com>
2026-02-28 02:47:16 +0530

    Refactored frontend for production: removed localhost, added api config, fixed routing

--- e-commerce/src/i18n.js
+++ e-commerce/src/i18n.js
@@ -1,29 +1,24 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import LanguageDetector from "i18next-browser-languagedetector";
 
-import enCommon from "./locales/en/common.json";
-import hiCommon from "./locales/hi/common.json";
-import knCommon from "./locales/kn/common.json";
+import en from "./locales/en/common.json";
+import hi from "./locales/hi/common.json";
+import kn from "./locales/kn/common.json";
+import ta from "./locales/ta/common.json";
+import te from "./locales/te/common.json";
 
 i18n
-  .use(LanguageDetector)
   .use(initReactI18next)
   .init({
     resources: {
-      en: {
-        common: enCommon
-      },
-      hi: {
-        common: hiCommon
-      },
-      kn: {
-        common: knCommon
-      }
+      en: { translation: en },
+      hi: { translation: hi },
+      kn: { translation: kn },
+      ta: { translation: ta },
+      te: { translation: te }
     },
+    lng: localStorage.getItem("language") || "en",
     fallbackLng: "en",
-    ns: ["common"],
-    defaultNS: "common",
     interpolation: {
       escapeValue: false
     }
